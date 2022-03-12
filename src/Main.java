65ec7bca871f465059db7b329b6a249524cf21a2
diogo <diogoguerra578@gmail.com>
2022-03-12 11:04:56 +0000

    Ficha 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public Main(){
+        //criar instancias
+        Professor professor1 =
+                new Professor("Sonia Luz", 1);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}

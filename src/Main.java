7cff10f1f3f8f18d94a4c0e361321adca66e9fbe
Diogo Guerra <63842251+diogoguerra03@users.noreply.github.com>
2022-03-12 11:26:53 +0000

    TPC

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         //criar instancias
         Professor professor1 =
                 new Professor("Sonia Luz", 1);
+        Aluno aluno1 =
+                new Aluno("Diogo Guerra",2211027);
+        Aula aula1 =
+                new Aula("Programação 2", 001);
     }
 
     public static void main(String[] args) {

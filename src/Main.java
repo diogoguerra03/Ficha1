12e34032e28f30ecf21c2d5a1b96a6caac6f7ef9
diogo <diogoguerra578@gmail.com>
2022-03-17 11:51:50 +0000

    Ficha 1 - 17/03

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
+import modelo.Aluno;
+import modelo.Aula;
+import modelo.Professor;
+
 public class Main {
 
     public Main(){
         //criar instancias
+        //Professores
         Professor professor1 =
                 new Professor("Sonia Luz", 1);
-        Aluno aluno1 =
-                new Aluno("Diogo Guerra",2211027);
+        Professor professor2 =
+                new Professor("Carlos Urbano", 2);
+
+        //modelo.Aula
         Aula aula1 =
-                new Aula("Programação 2", 001);
+                new Aula("Programação 2 PL5", 1);
+        Aula aula2 =
+                new Aula("Programação 2 TP3", 1);
+
+        //Alunos
+        Aluno aluno1 =
+                new Aluno("Adriano Gomes",2211030);
+        Aluno aluno2 =
+                new Aluno("Bárbara Silva", 2210903);
     }
 
     public static void main(String[] args) {
